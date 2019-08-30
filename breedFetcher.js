aa9789023aed5721d8c06179d87b944c56f030a8
siningtong <siningtong@gmail.com>
2019-08-30 00:48:25 +0000

    tested

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,15 @@
 const request = require('request');
-
-const args = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`,(erro,response,body) => {
-  if (erro) {
-    console.log('error:', erro);
-  }
-  const data = JSON.parse(body);
-  console.log('statusCode:', response && response.statusCode);
-  if (data.length === 0) {
-    console.log('The requested breed is not found');
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,(err,response,body) => {
+    if (err) {
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('The requested breed is not found',null);
+    } else {
+      callback(null, data[0].description.trim());
+    }
+  });
+};
+module.exports = { fetchBreedDescription };
